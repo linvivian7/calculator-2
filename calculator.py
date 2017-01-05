9b79667d7342558de3bdaf2e3c7b84eb7166fd12
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 17:57:21 -0800

    Implement calculator with reduceand exceptions

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
+from arithmetic1 import *
 
 
 # Map between user input and calculator functions
@@ -20,27 +20,56 @@ function_mappings = {
     "mod": mod
 }
 
+
+def compute(math_operator, numbers):
+    """Calculate function with variable args.
+
+    args:
+        math_operator: add, subtract, multiply, divide,
+            square, cube, power, and mod (see arithmetic1.py)
+        numbers: a list of numbers to iterate over
+    returns:
+        The computed math function on the numeric list
+    """
+    return reduce(lambda x, y: math_operator(int(x), int(y)), numbers)
+
+
 # Perform calculations until the user types 'q'
 while True:
     # Prompt user for input
-    input_string = raw_input(">")
+    input_string = raw_input(">").rstrip()
 
     # Tokenize input on spaces
     tokens = input_string.split(" ")
     func = tokens[0]
 
-    try:
-        if tokens[0] == "q":
-            break
-
-        # Calculate functions that have 1 arg
-        elif len(tokens) == 2:
-            print function_mappings[func](int(tokens[1]))
-
-        # Calculate functions that have 2 args
-        elif len(tokens) == 3:
-            print function_mappings[func](int(tokens[1]), int(tokens[2]))
-
-    except:
-        print "That is an invalid input"
+    # Quit program
+    if tokens[0] == "q":
+        break
+    # Give error if operator is not defined
+    elif func not in function_mappings:
+        print "Please input a valid operator"
         continue
+    
+    # Print out error messages for incorrect number of operands
+    if func in ["+", "-", "*", "/"]:
+        if len(tokens) < 3:
+            print "Please enter at least 2 operands"
+            continue
+    elif func in ["square", "cube"]:
+        if len(tokens) > 2:
+            print "Please only input 1 operand"
+            continue
+    elif func in ["pow", "mod"]:
+        if len(tokens) != 3:
+            print "Please enter only 2 operands"
+            continue
+
+    try:
+        # Run compute function starting at index 1
+        print compute(function_mappings[func], tokens[1:])
+
+    # Expect TypeError when non-integers are entered after index 1
+    # Expect ValueError when math operation cannot be completed (i.e. dividing by 0)
+    except TypeError, ValueError:
+        print "That is an invalid input"
